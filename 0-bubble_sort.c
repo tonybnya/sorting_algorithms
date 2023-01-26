eb2e4b65c069c42862c9dcccad1390576cbc078d
Tony <nya.tony2010@gmail.com>
2023-01-26 10:58:17 +0100

    update

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,7 +1,5 @@
 #include "sort.h"
 
-void swap(int *first, int *second);
-
 /**
  * bubble_sort - Bubble Sort Algorithm
  *
@@ -31,18 +29,4 @@ void bubble_sort(int *array, size_t size)
 		if (!swapped)
 			break;
 	}
-}
-
-/**
- * swap - swap two integers using pointers
- * @first: pointer to the first integer
- * @second: pointer to the second integer
- */
-void swap(int *first, int *second)
-{
-	int temp;
-
-	temp = *first;
-	*first = *second;
-	*second = temp;
 }
---
+++ 2-selection_sort.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+
+/* void selectioovoidn_sort(int *array, size_t size); */
+void swap(int *first, int *second);
+
+/**
+ * selection_sort - Selection Sort Algorithm
+ *
+ * @array: Array to be sorted
+ * @size: size of th @array
+ */
+void selection_sort(int *array, size_t size)
+{
+        size_t i, j, min_idx;
+
+        if (size < 2)
+                return;
+
+        for (i = 0; i < size; i++)
+        {
+                min_idx = i;
+                for (j = i + 1; j < size; j++)
+                {
+                        if (array[j] < array[min_idx])
+                                min_idx = j;
+
+                        if (min_idx != i)
+                        {
+                                swap(&array[min_idx], &array[i]);
+                                print_array(array, size);
+                        }
+                }
+        }
+}
