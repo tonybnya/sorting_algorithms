47eec7b2382906208f12417da93896d459eddaea
Tony Blondeau NYA <nya.tony2010@gmail.com>
2023-01-26 01:30:00 -0800

    update

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -7,36 +7,36 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-        listint_t *node, *head;
+	listint_t *node, *head;
 
-        if (!list)
-                return;
+	if (!list)
+		return;
 
-        head = *list;
-        while (head)
-        {
-                while (head->next && (head->n > head->next->n))
-                {
-                        node = head->next;
-                        head->next = node->next;
-                        node->prev = head->prev;
+	head = *list;
+	while (head)
+	{
+		while (head->next && (head->n > head->next->n))
+		{
+			node = head->next;
+			head->next = node->next;
+			node->prev = head->prev;
 
-                        if (head->prev)
-                                head->prev->next = node;
+			if (head->prev)
+				head->prev->next = node;
 
-                        if (node->next)
-                                node->next->prev = head;
+			if (node->next)
+				node->next->prev = head;
 
-                        head->prev = node;
-                        node->next = head;
+			head->prev = node;
+			node->next = head;
 
-                        if (node->prev)
-                                head = node->prev;
-                        else
-                                *list = node;
+			if (node->prev)
+				head = node->prev;
+			else
+				*list = node;
 
-                        print_list(*list);
-                }
-                head = head->next;
-        }
+			print_list(*list);
+		}
+		head = head->next;
+	}
 }
