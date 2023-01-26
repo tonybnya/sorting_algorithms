c9e3858a9831a41ef082120372304d535b78f543
Tony Blondeau NYA <nya.tony2010@gmail.com>
2023-01-26 02:00:45 -0800

    update

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -11,24 +11,24 @@ void swap(int *first, int *second);
  */
 void selection_sort(int *array, size_t size)
 {
-        size_t i, j, min_idx;
+	size_t i, j, min_idx;
 
-        if (size < 2)
-                return;
+	if (size < 2)
+		return;
 
-        for (i = 0; i < size; i++)
-        {
-                min_idx = i;
-                for (j = i + 1; j < size; j++)
-                {
-                        if (array[j] < array[min_idx])
-                                min_idx = j;
+	for (i = 0; i < size; i++)
+	{
+		min_idx = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_idx])
+				min_idx = j;
 
-                        if (min_idx != i)
-                        {
-                                swap(&array[min_idx], &array[i]);
-                                print_array(array, size);
-                        }
-                }
-        }
+			if (min_idx != i)
+			{
+				swap(&array[min_idx], &array[i]);
+				print_array(array, size);
+			}
+		}
+	}
 }
