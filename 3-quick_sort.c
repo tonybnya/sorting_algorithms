e4ab92d94a6e7a140e20ff92ba50201811bb7741
Tony Blondeau NYA <nya.tony2010@gmail.com>
2023-01-28 03:53:41 -0800

    update

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -12,47 +12,48 @@ void sort(int array[], int low, int high, int size);
  */
 void quick_sort(int *array, size_t size)
 {
-        sort(array, 0, size - 1, size);
+	sort(array, 0, size - 1, size);
 }
 
 void swap(int *first, int *second)
 {
-        int temp;
+	int temp;
 
-        temp = *first;
-        *first = *second;
-        *second = temp;
+	temp = *first;
+	*first = *second;
+	*second = temp;
 }
 
 int partition(int array[], int low, int high, int size)
 {
-        int pivot;
-        int i, j;
+	int pivot;
+	int i, j;
 
-        pivot = array[high];
-        i = (low - 1);
+	pivot = array[high];
+	i = (low - 1);
 
-        for (j = low; j <= high - 1; j++)
-        {
-                if (array[j] <= pivot)
-                {
-                        i++;
-                        swap(&array[i], &array[j]);
-                        print_array(array, size);
-                }
-        }
-        swap(&array[i + 1], &array[high]);
-        print_array(array, size);
+	for (j = low; j <= high - 1; j++)
+	{
+		if (array[j] <= pivot)
+		{
+			i++;
+			swap(&array[i], &array[j]);
+			print_array(array, size);
+		}
+	}
+	swap(&array[i + 1], &array[high]);
+	print_array(array, size);
 
-        return (i + 1);
+	return (i + 1);
 }
 
 void sort(int array[], int low, int high, int size)
 {
-        if (low < high)
-        {
-                int pi = partition(array, low, high, size);
-                sort(array, low, pi - 1, size);
-                sort(array, pi + 1, high, size);
-        }
+	if (low < high)
+	{
+		int pi = partition(array, low, high, size);
+
+		sort(array, low, pi - 1, size);
+		sort(array, pi + 1, high, size);
+	}
 }
